5f0ebf41e45cc0d213c26535db8b12534909d2ac
Gyuhyeok <ghkdrbgur13@naver.com>
2023-12-29 22:22:56 +0900

    feat : UserController 기능 추가

---
+++ src/main/java/jwt/security/user/UserController.java
@@ -0,0 +1,24 @@
+package jwt.security.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PatchMapping
+    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordReq request, Principal connectedUser) {
+        userService.changePassword(request, connectedUser);
+        return ResponseEntity.ok().build();
+    }
+}
