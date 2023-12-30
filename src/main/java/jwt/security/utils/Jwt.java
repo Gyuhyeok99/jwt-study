073c3ee41ddf9e7f0c89684035b99b0520ec4035
Gyuhyeok <ghkdrbgur13@naver.com>
2023-12-30 23:16:34 +0900

    refactor : 이메일 인증 메시지 형식 리펙토링

--- src/main/java/jwt/security/utils/Jwt.java
+++ src/main/java/jwt/security/utils/Jwt.java
@@ -9,9 +9,9 @@ public class Jwt {
 
     public final static String EMAIL_CONTENT_PREFIX = "JWT TEST용 프로젝트 로그인 인증입니다." +
             "<br><br>" +
-            "인증 번호는 ";
-
-    public final static String EMAIL_CONTENT_SUFFIX = "입니다." +
+            "인증 번호는 <strong><i>&lt;여기에인증번호&gt;</i></strong> 입니다." +
             "<br>" +
             "인증번호를 제대로 입력해주세요";
+
+    public final static String EMAIL_CONTENT_SUFFIX = "";
 }
